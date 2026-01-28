1c54609dcc8204f6cfc4841a3f28e5e941f2a4ad
tom36596263 <tom36596263@gmail.com>
2026-01-28 15:43:53 +0800

    Initial commit with connection samples

---
+++ README.md
@@ -0,0 +1,1 @@
+Recimo 後端 API 專案。詳細開發文件與 API 使用說明請參考我們的Notion文件：[https://www.notion.so/2f672d13d8bf804dba18f79891e3d5ff?source=copy_link]
---
+++ db_config.sample.php
@@ -0,0 +1,17 @@
+<?php
+$db_host = '';               // 資料庫主機(ip)
+$db_port = 3306;                      // 資料庫 port number
+$db_dbname = 'tibamefe_cjd102g2'; // 資料庫名稱 (需更改)
+
+$db_user = '';      // 可連線資料庫的帳號
+$db_password = '';        // 該帳號的密碼
+
+$dsn = "mysql:host=$db_host;port=$db_port;dbname=$db_dbname;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $db_user, $db_password);
+    echo '<p style="color: green;">資料庫連線成功。</p>';
+} catch (PDOException $e) {
+    echo '<p style="color: red;">資料庫連線錯誤：' . $e->getMessage() . '。</p>';
+    exit();
+}
