5ed645a22fb04a2295ef1afa7a1d02232ccdad6d
wei-yan-chen <h153123321@gmail.com>
2026-02-09 16:03:22 +0800

    Fix:修食譜卡片 顯示資訊

--- recipes/recipe_base_sql.php
+++ recipes/recipe_base_sql.php
@@ -5,10 +5,15 @@ $baseSelect = "SELECT
     p.product_category,
     p.product_image,
     p.product_id AS linked_product_id,
+    u.user_id AS author_id,
+    u.user_name AS author_name,
+    COALESCE(u.user_url, 'img/site/None_avatar.svg') AS author_image,
+    u.user_email,
     GROUP_CONCAT(DISTINCT t.tag_name) AS tag_names,
     GROUP_CONCAT(DISTINCT ri.ingredient_id) AS ingredient_ids
 FROM recipes r
 LEFT JOIN products p ON r.linked_product_id = p.product_id
+LEFT JOIN users u ON r.author_id = u.user_id
 LEFT JOIN recipe_tag rt ON r.recipe_id = rt.recipe_id
 LEFT JOIN tags t ON rt.tag_id = t.tag_id
 LEFT JOIN recipe_ingredients ri ON r.recipe_id = ri.recipe_id";
